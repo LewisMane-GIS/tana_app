a37aacd8e1a3600ec0888ceebd54607622da83de
LewisGIS <96820204+LewisMane-GIS@users.noreply.github.com>
2023-10-26 15:22:25 +0300

    Update 1

--- app1.py
+++ app1.py
@@ -24,7 +24,7 @@ st.write("""
 *click on a marker to view project details*
 """)
 
-HtmlFile = open("./maps/tana_map1.html", 'r')
+HtmlFile = open("./maps/new_map2.html", 'r')
 Tana_map = HtmlFile.read()
 components.html(Tana_map, height=700)
 
@@ -83,6 +83,12 @@ col3.markdown("""
 </span> =
 <span style='color:green;'>
 Planning and Design stage 
+</span><br>
+<span style='color:green;'>
+. Other 
+</span> =
+<span style='color:green;'>
+Other Projects 
 </span><br>
 """,
               unsafe_allow_html=True)
